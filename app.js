a2689a4b850f718a8177153592bccf24917b799c
adam-kollberg <adam@web-it.nu>
2020-12-01 14:09:12 +0100

    added more JS

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const productName = document.querySelector("#product_name").value;
 const productDescription = document.querySelector("#product_description").value;
 const productPrice = document.querySelector("#product_price").value;
 
+const productDiv = document.querySelector("product_container");
+
+const createdDiv = document.createElement("div");
+
+createdDiv.appendChild("productDiv");
 
 }
-
-
-
 
 // välja knappen "add product"
 const btn_add_product = document.querySelector(".add_product");
 
 // EventListner till addproduct function
 btn_add_product.addEventListener("click", addProduct);
-
-
