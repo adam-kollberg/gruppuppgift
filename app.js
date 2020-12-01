e2338d829e41058edae3b4b35536bfb0dc9e4258
adam-kollberg <adam@web-it.nu>
2020-12-01 12:57:06 +0100

    added variables for the product values and installed sass

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+
+function addProduct (e) {
+e.preventDefault()
+
+// Välja det olika värderna
+const productName = document.querySelector("#product_name").value;
+const productDescription = document.querySelector("#product_description").value;
+const productPrice = document.querySelector("#product_price").value;
+
+
+}
+
+
+
+
+// välja knappen "add product"
+const btn_add_product = document.querySelector(".add_product");
+
+// EventListner till addproduct function
+btn_add_product.addEventListener("click", addProduct);
+
+
