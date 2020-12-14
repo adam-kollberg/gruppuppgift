afc11a2f6f8d31ed4de7296ea730e50f2eb55e3f
adam-kollberg <adam@web-it.nu>
2020-12-14 11:26:03 +0100

    added products to frontpage

--- addProduct.js
+++ addProduct.js
@@ -1,134 +1,33 @@
 
 
-function addProduct (e) {
-    e.preventDefault()
-    
-    
-    
-    
-    
-    
-    // Välja det olika värderna
-   
-    const productName = document.querySelector("#product_name").value;
-    const productDescription = document.querySelector("#product_description").value;
-    const productPrice = document.querySelector("#product_price").value;
-    
-    
-    //create Div
-    const createdDiv = document.createElement ("div");
-    const productDiv = document.querySelector(".product_container");
-    productDiv.appendChild(createdDiv);
-    createdDiv.className += "single_product";
-    
-    // Create img
-    
-   
-const createdImg = document.createElement ("img");
-createdDiv.appendChild(createdImg);
-createdImg.src= "bilder/chanel.jpeg";
-createdImg.className += "product_img";
-    
-    //create H3 for title
-    const createdTitle = document.createElement ("h3");
-    createdDiv.appendChild(createdTitle);
-    createdTitle.innerText= productName;
-    createdTitle.className += "product_name";
-    
-    //create p for description
-    const createdDescription = document.createElement ("p");
-    createdDiv.appendChild(createdDescription);
-    createdDescription.innerText= productDescription;
-    createdDescription.className += "product_description";
-    
-    // Create p for price
-    const createdPrice = document.createElement ("p");
-    createdDiv.appendChild(createdPrice);
-    createdPrice.innerText= productPrice + ":-";
-    createdPrice.className += "product_price";
-    
-    
-    // Create button for add to cart
-    const createAddToCartBtn = document.createElement("button");
-    createdDiv.appendChild(createAddToCartBtn);
-    createAddToCartBtn.innerText= "Add to cart";
-    
+const addProductBtn = document.querySelector(".add_product")
 
-    //Create remove-button
-    const createRemoveCartBtn = document.createElement("button");
-    createdDiv.appendChild(createRemoveCartBtn);
-    createRemoveCartBtn.innerText = "Remove product";
-    createRemoveCartBtn.className += "remove_product";
+addProductBtn.addEventListener("click", passProducts);
+
+let products = []
 
 
 
-
-// Remove product
-
-    let removeBtn = document.querySelector(".remove_product");
-     
-    removeBtn.addEventListener("click", removeProduct);
-
-    function removeProduct() {
-
-        createdDiv.remove();
-    }
-    
-    
-    }
-    
-    // Added event listner
-    
-    const btn_add_product = document.querySelector(".add_product");
-    
-    btn_add_product.addEventListener("click", addProduct);
-
-    
-
-
-
-
-    
-
+function passProducts(e) { 
+let productObject = {}  
   
-    
-   const images = []
-
-   index = 0;
-images[0] = "/Users/nicklassegerdahl/REPOS/gruppuppgift/Images/picture1.jpg";
-images[1] = "/Users/nicklassegerdahl/REPOS/gruppuppgift/Images/picture2.jpg";
-images[2] = "/Users/nicklassegerdahl/REPOS/gruppuppgift/Images/picture3.jpg";
-index = Math.floor(Math.random() * images.length);
-document.write(images[index]);
+const productName = document.querySelector("#product_name").value;
+const productDescription = document.querySelector("#product_description").value;
+const productPrice = document.querySelector("#product_price").value;
 
 
-//done
-
-    /* function createImageBase(src, alt) {
-        var image = document.createElement('img');
-        image.src = src;
-        image.alt = alt;
-        return image;
-    }
-    
-    
-    function addPicture( ){
-       var location = document.getElementById("#file");
-       var image = createImageBase(imageSRC, imageSRC);
-       location.appendChild(image);
-    }
-    
- */
+productObject.name = productName;
+productObject.description = productDescription;
+productObject.price = productPrice;
 
 
-    /* function loadimg() {
-        const file = document.getElementById('file').files[0];
-        const reader  = new FileReader();
-        reader.onload = function(e)  {
-            const image = document.createElement("img");
-            image.src = e.target.result;
-            document.body.appendChild(image);
-         }
-         reader.readAsDataSrc(file);  
-        }  
-          */
+products.push(productObject);
+const localData = localStorage.getItem("produkt");
+
+const existingData = JSON.parse(localData);
+
+const cleanedData = existingData ?  existingData.concat(products) : products;
+
+localStorage.setItem("produkt", JSON.stringify(cleanedData));
+
+}
